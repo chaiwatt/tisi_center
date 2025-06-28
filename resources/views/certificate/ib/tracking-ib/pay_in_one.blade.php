bb7a53a7579c043e3f0f3671cad34d8eb9087b80
chaiwatt <chaiwat_thaweechan@yahoo.com>
2025-06-28 08:00:35 +0700

    update

--- resources/views/certificate/ib/tracking-ib/pay_in_one.blade.php
+++ resources/views/certificate/ib/tracking-ib/pay_in_one.blade.php
@@ -308,9 +308,9 @@ $SumCost = !empty($pay_in->auditors_to->SumCostConFirm) ? $pay_in->auditors_to->
 </div>
 @endif
 
-@php
+{{-- @php
     dd($pay_in);
-@endphp
+@endphp --}}
 
 @if($pay_in->state == null || $pay_in->state == 2)
 <input type="hidden" name="previousUrl" id="previousUrl" value="{{   app('url')->previous() }}">
