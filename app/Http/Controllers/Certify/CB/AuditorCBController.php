d62a3dc8cb8d08e87ce9b9be6bf9293e04d186f4
chaiwatt <chaiwat_thaweechan@yahoo.com>
2025-07-29 12:29:28 +0700

    update

--- app/Http/Controllers/Certify/CB/AuditorCBController.php
+++ app/Http/Controllers/Certify/CB/AuditorCBController.php
@@ -242,13 +242,14 @@ class AuditorCBController extends Controller
                     }
                 }
                 
-              return redirect()->route('check_certificate-cb.index');
-                // if($request->previousUrl){
-                //   return redirect("$request->previousUrl")->with('flash_message', 'เรียบร้อยแล้ว!');
-                // }else{
-                //     return redirect('certify/auditor-cb')->with('flash_message', 'เรียบร้อยแล้ว!');
-                // }
+              // return redirect()->route('check_certificate-cb.index');
+                if($request->previousUrl){
+                  return redirect("$request->previousUrl")->with('flash_message', 'เรียบร้อยแล้ว!');
+                }else{
+                    return redirect('certify/auditor-cb')->with('flash_message', 'เรียบร้อยแล้ว!');
+                }
           } catch (\Exception $e) {
+            dd($e);
                  return redirect('certify/auditor-cb')->with('message_error', 'เกิดข้อผิดพลาดกรุณาทำรายการใหม่!');
           }
 
