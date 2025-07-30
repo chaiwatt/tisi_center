0a75bd9ef7c4aee750cee97479099199ac1b4304
chaiwatt <chaiwat_thaweechan@yahoo.com>
2025-07-30 18:36:57 +0700

    update

--- app/Http/Controllers/Certify/SendCertificatesController.php
+++ app/Http/Controllers/Certify/SendCertificatesController.php
@@ -612,10 +612,10 @@ class SendCertificatesController extends Controller
                     foreach($export as $key => $item){
                         if(!empty($item->CertiCbTo)){
                             $send_cer_list = SendCertificateLists::select('id','sign_status')->where('certificate_id',$item->id)->where('certificate_tb',$table)->first();
-
+   dd($send_cer_list);
                             // เงื่อนไข: ใบรับรองยังไม่เคยถูกส่ง หรือถูกยกเลิกการส่ง (สถานะ 4)
                             if(is_null($send_cer_list) || (!is_null($send_cer_list) && $send_cer_list->sign_status == 4)){
-                                dd("stupid");
+                             
                                 $cb = $item->CertiCbTo;
                                 $list = (object)[];
                                 $list->id = $item->id;
