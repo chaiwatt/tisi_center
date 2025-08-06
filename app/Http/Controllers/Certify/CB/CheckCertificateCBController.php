f635bec0c7d7de50a487648d894fc9d7994c2015
chaiwatt <chaiwat_thaweechan@yahoo.com>
2025-08-06 12:37:32 +0700

    update

--- app/Http/Controllers/Certify/CB/CheckCertificateCBController.php
+++ app/Http/Controllers/Certify/CB/CheckCertificateCBController.php
@@ -138,7 +138,7 @@ class CheckCertificateCBController extends Controller
                 // dd(auth()->user()->RoleListId);
                 $check = CertiCBCheck::where('user_id',auth()->user()->runrecno)->pluck('app_certi_cb_id'); // เช็คเจ้าหน้าที่ IB
                 if(isset($check) && count($check) > 0  ) {
-                    dd($check->latest()->first());
+                    dd(count($check));
                      $Query = $Query->LeftJoin('app_certi_cb_check','app_certi_cb_check.app_certi_cb_id','=','app_certi_cb.id')
                                     ->where('user_id',auth()->user()->runrecno);  //เจ้าหน้าที่  IB ที่ได้มอบหมาย
                 }else{
