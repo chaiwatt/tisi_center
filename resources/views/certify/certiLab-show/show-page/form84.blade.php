c6a8759fd0881479a0e866b5c08a54bf16d9cea6
chaiwatt <chaiwat_thaweechan@yahoo.com>
2025-08-26 12:30:48 +0700

    update

--- resources/views/certify/certiLab-show/show-page/form84.blade.php
+++ resources/views/certify/certiLab-show/show-page/form84.blade.php
@@ -31,7 +31,7 @@
                 <div class="row">
                     <div class="col-md-8">
                         <div class="form-group">
-                            ชื่อผู้ยื่นขอรับรองการรับรอง: <label for="app_name">{{  !empty($certi_lab->name) ?  $certi_lab->name :   $certi_information->name }}</label>
+                            ชื่อผู้ยื่นขอรับรองการรับรอง: <label for="app_name">{{  !empty($certi_lab->name) ?  $certi_lab->name :   @$certi_information->name }}</label>
                         </div>
                     </div>
                 </div>
@@ -40,7 +40,7 @@
                 <div class="row">
                     <div class="col-md-8">
                         <div class="form-group">
-                            เลขประจำตัวผู้เสียภาษีอากร: <label for="id_tax">{{ $certi_information->tax_indentification_number }}</label>
+                            เลขประจำตัวผู้เสียภาษีอากร: <label for="id_tax">{{ @$certi_information->tax_indentification_number }}</label>
                         </div>
                     </div>
                 </div>
@@ -50,12 +50,12 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="form-group">
-                            อายุ: <label for="app_old">{{ $certi_information->ages }}</label>
+                            อายุ: <label for="app_old">{{ @$certi_information->ages }}</label>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
-                            สัญชาติ: <label for="app_nation">{{ $certi_information->nationality }}</label>
+                            สัญชาติ: <label for="app_nation">{{ @$certi_information->nationality }}</label>
                         </div>
                     </div>
                 </div>
@@ -64,37 +64,17 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="form-group">
-                            อยู่บ้านเลขที่: <label for="home_num">{{ $certi_information->address_no }}</label>
+                            อยู่บ้านเลขที่: <label for="home_num">{{ @$certi_information->address_no }}</label>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
-                            ตรอก/ซอย: <label for="home_soi">{{ $certi_information->alley }}</label>
+                            ตรอก/ซอย: <label for="home_soi">{{ @$certi_information->alley }}</label>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
-                            ถนน: <label for="home_street">{{ $certi_information->road }}</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-12">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            หมู่ที่: <label for="home_moo">{{ $certi_information->village_no }}</label>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            จังหวัด: <label for="home_province">{{ $certi_information->province }}</label>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            อำเภอ/เขต: <label for="home_area" >{{ $certi_information->amphur }}</label>
+                            ถนน: <label for="home_street">{{ @$certi_information->road }}</label>
                         </div>
                     </div>
                 </div>
@@ -104,17 +84,37 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="form-group">
-                            ตำบล/แขวง: <label for="home_tumbon" >{{ $certi_information->district }}</label>
+                            หมู่ที่: <label for="home_moo">{{ @$certi_information->village_no }}</label>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
-                            ไปรษณีย์: <label for="home_post">{{ $certi_information->postcode }}</label>
+                            จังหวัด: <label for="home_province">{{ @$certi_information->province }}</label>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
-                            โทรศัพท์: <label for="home_phone">{{ $certi_information->tel }}</label>
+                            อำเภอ/เขต: <label for="home_area" >{{ @$certi_information->amphur }}</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            ตำบล/แขวง: <label for="home_tumbon" >{{ @$certi_information->district }}</label>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            ไปรษณีย์: <label for="home_post">{{ @$certi_information->postcode }}</label>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            โทรศัพท์: <label for="home_phone">{{ @$certi_information->tel }}</label>
                         </div>
                     </div>
                 </div>
@@ -123,7 +123,7 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            เลขประจำตัวผู้เสียภาษีอากร: <label for="id_tax">{{ $certi_information->tax_indentification_number }}</label>
+                            เลขประจำตัวผู้เสียภาษีอากร: <label for="id_tax">{{ @$certi_information->tax_indentification_number }}</label>
                         </div>
                     </div>
                 </div>
@@ -133,7 +133,7 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="form-group">
-                            มีสำนักงานใหญ่ตั้งอยู่เลขที่ : <label for="head_num">{{ $certi_information->address_headquarters }}</label>
+                            มีสำนักงานใหญ่ตั้งอยู่เลขที่ : <label for="head_num">{{ @$certi_information->address_headquarters }}</label>
                         </div>
                     </div>
                 </div>
@@ -143,37 +143,17 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="form-group">
-                            ตรอก/ซอย :  <label for="head_soi">{{ $certi_information->headquarters_alley }}</label>
+                            ตรอก/ซอย :  <label for="head_soi">{{ @$certi_information->headquarters_alley }}</label>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
-                            ถนน : <label for="head_street">{{ $certi_information->headquarters_road }}</label>
+                            ถนน : <label for="head_street">{{ @$certi_information->headquarters_road }}</label>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
-                            หมู่ที่ : <label for="head_moo">{{ $certi_information->headquarters_village_no }}</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-12">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            ตำบล/แขวง : <label for="head_tumbon">{{ $certi_information->headquarters_district }}</label>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            อำเภอ/เขต : <label for="head_area">{{ $certi_information->headquarters_amphur }}</label>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            จังหวัด : <label for="head_province">{{ $certi_information->headquarters_province }}</label>
+                            หมู่ที่ : <label for="head_moo">{{ @$certi_information->headquarters_village_no }}</label>
                         </div>
                     </div>
                 </div>
@@ -183,17 +163,37 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="form-group">
-                            รหัสไปรษณีย์ : <label for="head_post">{{ $certi_information->headquarters_postcode }}</label>
+                            ตำบล/แขวง : <label for="head_tumbon">{{ @$certi_information->headquarters_district }}</label>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
-                            โทรศัพท์ : <label for="head_tel">{{ $certi_information->headquarters_tel }}</label>
+                            อำเภอ/เขต : <label for="head_area">{{ @$certi_information->headquarters_amphur }}</label>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-group">
-                            โทรสาร: <label for="head_fax">{{ $certi_information->headquarters_tel_fax }}</label>
+                            จังหวัด : <label for="head_province">{{ @$certi_information->headquarters_province }}</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            รหัสไปรษณีย์ : <label for="head_post">{{ @$certi_information->headquarters_postcode }}</label>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            โทรศัพท์ : <label for="head_tel">{{ @$certi_information->headquarters_tel }}</label>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            โทรสาร: <label for="head_fax">{{ @$certi_information->headquarters_tel_fax }}</label>
                         </div>
                     </div>
                 </div>
@@ -203,7 +203,7 @@
                 <div class="row">
                     <div class="col-md-5">
                         <div class="form-group">
-                            จดทะเบียนเป็นนิติบุคคลเมื่อวันที่: <label for="entity_date">{{  HP::DateThai($certi_information->date_regis_juristic_person) ?? '-' }}</label>
+                            จดทะเบียนเป็นนิติบุคคลเมื่อวันที่: <label for="entity_date">{{  HP::DateThai(@$certi_information->date_regis_juristic_person) ?? '-' }}</label>
                         </div>
                     </div>
                     {{-- <div class="col-md-4">
